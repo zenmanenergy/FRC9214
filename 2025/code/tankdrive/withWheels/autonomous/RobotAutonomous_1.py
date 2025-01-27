045930008a7fc2065f6ebf896941a76279c7b680
roadsteadsteve <70551331+roadsteadsteve@users.noreply.github.com>
2025-01-27 18:48:22 -0500

    library updates

--- 2025/code/tankdrive/withWheels/autonomous/RobotAutonomous_1.py
+++ 2025/code/tankdrive/withWheels/autonomous/RobotAutonomous_1.py
@@ -6,21 +6,24 @@ during the autonomous phase. The drive function resets automatically for subsequ
 """
 
 import wpilib
-import ctre
+from phoenix5 import WPI_TalonSRX
+
 
 class MyRobot(wpilib.TimedRobot):
 	def robotInit(self):
-		self.LeftFrontMotor = ctre.WPI_TalonSRX(1)
-		self.LeftRearMotor = ctre.WPI_TalonSRX(2)
-		self.RightFrontMotor = ctre.WPI_TalonSRX(3)
-		self.RightRearMotor = ctre.WPI_TalonSRX(4)
+		self.LeftFrontMotor = WPI_TalonSRX(1)
+		self.LeftRearMotor = WPI_TalonSRX (4)
+
+		self.RightFrontMotor = WPI_TalonSRX(3)
+		self.RightRearMotor = WPI_TalonSRX(2)
+
 		self.timer = wpilib.Timer()
 
 	def autonomousInit(self):
 		self.timer.reset()
 
 	def autonomousPeriodic(self):
-		self.drive_for_time(0.4, 0.75)
+		self.drive_for_time(0.4, 2.5)
 
 	def teleopInit(self):
 		pass
@@ -31,10 +34,10 @@ class MyRobot(wpilib.TimedRobot):
 	def drive_for_time(self, speed, duration):
 		if self.timer.get() == 0:
 			self.timer.start()
-			self.LeftFrontMotor.set(speed)
-			self.LeftRearMotor.set(speed)
-			self.RightFrontMotor.set(-speed)
-			self.RightRearMotor.set(-speed)
+			self.LeftFrontMotor.set(-speed)
+			self.LeftRearMotor.set(-speed)
+			self.RightFrontMotor.set(speed)
+			self.RightRearMotor.set(speed)
 
 		if self.timer.get() >= duration:
 			self.LeftFrontMotor.set(0)
--- deploy/robot.py
+++ deploy/robot.py
@@ -8,30 +8,50 @@ of the autonomous routine.
 
 import wpilib
 from phoenix5 import WPI_TalonSRX
+
 class MyRobot(wpilib.TimedRobot):
 	def robotInit(self):
 		self.LeftFrontMotor = WPI_TalonSRX(1)
-		self.LeftRearMotor = WPI_TalonSRX(2)
+		self.LeftRearMotor = WPI_TalonSRX (4)
+
 		self.RightFrontMotor = WPI_TalonSRX(3)
-		self.RightRearMotor = WPI_TalonSRX(4)
+		self.RightRearMotor = WPI_TalonSRX(2)
 		self.timer = wpilib.Timer()
 		self.driveStep = 0
 
 	def autonomousInit(self):
+		self.timer.stop()
+		self.timer.reset()
 		self.driveStep = 0
 
 	def autonomousPeriodic(self):
 		if self.driveStep == 0:
-			if self.drive_forward_for_time(-0.4, 0.75):
+			if self.drive_forward_for_time(0.4, 2.4):
 				self.driveStep += 1
 
 		elif self.driveStep == 1:
-			if self.drive_forward_for_time(0.4, 0.75):
+			if self.drive_forward_for_time(-0.1, 0.25):
 				self.driveStep += 1
 
 		elif self.driveStep == 2:
-			if self.drive_forward_for_time(-0.05, 0.75):
+			if self.turn(-.4, 0.475):
 				self.driveStep += 1
+
+		elif self.driveStep == 3:
+			if self.turn(.05, 0.25):
+				self.driveStep += 1
+
+		elif self.driveStep == 4:
+			if self.drive_forward_for_time(0.4, 2.75):
+				self.driveStep += 1
+
+		elif self.driveStep == 5:
+			if self.drive_forward_for_time(-0.1, 0.25):
+				self.driveStep += 1
+
+		# elif self.driveStep == 6:
+		# 	if self.turn(-.4, 0.4):
+		# 		self.driveStep += 1
 
 	def teleopInit(self):
 		pass
@@ -55,8 +75,29 @@ class MyRobot(wpilib.TimedRobot):
 			self.timer.stop()
 			self.timer.reset()
 			return True
-
+		
 		return False
+	
+	def turn(self, speed, duration):
+		if self.timer.get() == 0:
+			self.timer.start()
+			self.LeftFrontMotor.set(speed)
+			self.LeftRearMotor.set(speed)
+			self.RightFrontMotor.set(speed)
+			self.RightRearMotor.set(speed)
+
+		if self.timer.get() >= duration:
+			self.LeftFrontMotor.set(0)
+			self.LeftRearMotor.set(0)
+			self.RightFrontMotor.set(0)
+			self.RightRearMotor.set(0)
+			self.timer.stop()
+			self.timer.reset()
+			return True
+		
+		return False
+
+
 
 if __name__ == "__main__":
 	wpilib.run(MyRobot)
