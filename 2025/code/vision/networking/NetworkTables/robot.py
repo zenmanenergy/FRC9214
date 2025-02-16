696f5521679238ed85ec0a06905545afbece432c
roadsteadsteve <70551331+roadsteadsteve@users.noreply.github.com>
2025-02-16 14:42:48 -0500

    network tables test

--- 2025/code/vision/networking/NetworkTables/robot.py
+++ 2025/code/vision/networking/NetworkTables/robot.py
@@ -4,17 +4,17 @@ from networktables import NetworkTables
 class MyRobot(wpilib.TimedRobot):
 	def robotInit(self):
 		"""Initialization code runs once when the robot powers on."""
-		NetworkTables.initialize()
-
+		NetworkTables.initialize(server="roborio-9214-frc.local")
 		table = NetworkTables.getTable("vision")
 
-		table.putNumber("x", 10)
+		table.putNumber("target_x", 10)
 
-		print(table.getNumber("x"))
+		print(table.getNumber("target_x", 0))
 
 	def teleopPeriodic(self):
 		"""Periodic code that runs during teleoperated control."""
-		
+		pass
+	 
 
 if __name__ == "__main__":
 	pass
