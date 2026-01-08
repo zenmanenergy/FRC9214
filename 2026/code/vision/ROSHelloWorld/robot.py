565ca5ebd16d634b4044ab06ae1a0a28bf164c0f
roadsteadsteve <70551331+roadsteadsteve@users.noreply.github.com>
2026-01-07 19:16:04 -0500

    ros

--- 2026/code/vision/ROSHelloWorld/robot.py
+++ 2026/code/vision/ROSHelloWorld/robot.py
@@ -3,15 +3,18 @@ from networktables import NetworkTables
 
 class MyRobot(wpilib.TimedRobot):
 	def robotInit(self):
-		# Initialize NetworkTables client to connect to the Ubuntu computer (10.92.14.100)
+		print("Robot Initialization")
+		# Initialize NetworkTables client to connect to the Ubuntu computer (10.92.14.200)
 		NetworkTables.initialize(server="10.92.14.200")
 		self.ros_table = NetworkTables.getTable("ros_data")
 		
-		print("[RoboRIO] Connecting to NetworkTables at 10.92.14.100")
+		print("[RoboRIO] Connecting to NetworkTables at 10.92.14.200")
 	
 	def teleopPeriodic(self):
+		
 		# Retrieve and print the message from the Ubuntu NetworkTables server
 		message = self.ros_table.getString("message", "")
+		
 		if message:
 			print(f"[RoboRIO] Received: {message}")
 		
